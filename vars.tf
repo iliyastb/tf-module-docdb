3681d1c81ae37a16c5f6deaf1a6f3b4e9ac66d49
iliyastb <skiliyas300@gmail.com>
2024-03-23 18:30:31 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -1,2 +1,9 @@
 variable "env" {}
+variable "tags" {}
+
 variable "engine" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "engine_version" {}
+variable "subnet_ids" {}
