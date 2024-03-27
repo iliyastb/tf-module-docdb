d25ce9ddc1d73adcfec0b0c54ce455b77089efd3
iliyastb <skiliyas300@gmail.com>
2024-03-27 18:11:09 +0530

    Update

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "aws_ssm_parameter" "user" {
-  name = "${var.env}-docdb.user"
+  name = "${var.env}.docdb.user"
 }
 
 data "aws_ssm_parameter" "pass" {
-  name = "${var.env}-docdb.pass"
+  name = "${var.env}.docdb.pass"
 }
 
 data "aws_kms_key" "key" {
